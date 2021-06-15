83c00d8475f1830f6209eeb99c90d2824b136846
Hossain Rabbi <hossainrabbi259@gmail.com>
2021-06-15 10:26:30 +0600

    redux-persist added

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,7 +1,19 @@
 import { createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
 import reducer from './reducer/reducer';
 
-const store = createStore(reducer, composeWithDevTools());
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: 'cartItems',
+};
+
+const persistedReducer = persistReducer(persistConfig, reducer);
+
+const store = createStore(persistedReducer, composeWithDevTools());
+
+export const persistor = persistStore(store);
 
 export default store;
